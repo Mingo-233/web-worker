23bfa8aadede93fefca849dfcb7c778599413919
mingo <mingo233@qq.com>
2022-05-14 20:09:37 +0800

    feat: web-worker

---
+++ public/sharedWorker.js
@@ -0,0 +1,28 @@
+// importScripts('constant.js'); // 引用其他文件
+let val = 6667;
+
+self.onconnect = function (e) {
+  const port = e.ports[0];
+  console.log("shared-worker connect");
+
+  port.postMessage(val);
+
+  port.onmessage = (messageEvent) => {
+    const { type, payload } = messageEvent.data;
+    console.log(type);
+    switch (type) {
+      case "increase":
+        port.postMessage(++val);
+        break;
+      case "decrease":
+        port.postMessage(--val);
+        break;
+      case "fresh":
+        console.log("fresh", val);
+        port.postMessage(val);
+        break;
+    }
+  };
+};
+
+console.log("shared-worker", self);
